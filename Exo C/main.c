08c56adb0bda19c71e6888953bb9dd9d66e7e116
Baptistee <baptiste.blct@gmail.com>
2019-10-22 16:59:03 +0200

    update

--- Exo C/main.c
+++ Exo C/main.c
@@ -3,9 +3,6 @@
 
 #define SIZE 10
 
-typedef struct sommet {
-	int num;
-}sommet;
 
 int* init_matrice()
 {
@@ -21,7 +18,21 @@ int* init_matrice()
 	return matrice;
 }
 
-void affiche(int* tab)
+int* init_liste()
+{
+	int i = 0;
+
+	int* liste = (int*)malloc((SIZE) * sizeof(int));
+
+	for (i; i < SIZE ; i++)
+	{
+		liste[i] = 0;
+	}
+
+	return liste;
+}
+
+void affiche_matrice(int* tab)
 {
 	int i = 0;
 
@@ -35,6 +46,22 @@ void affiche(int* tab)
 	}
 }
 
+void affiche_sommet(int *tab, int x, int y)
+{
+	printf("\n SOMMET = %d \n", tab[x * SIZE + y]);
+}
+
+void affiche_liste(int *liste)
+{
+	int i = 0;
+
+	while (liste[i] != 0)
+	{
+		printf("\n LISTE VAL = %d \n", liste[i]);
+		i++;
+	}
+}
+
 int* ajoute_sommet(int *matrice, int x, int y)
 {
 	matrice[x * SIZE + y] = 1;
@@ -43,17 +70,64 @@ int* ajoute_sommet(int *matrice, int x, int y)
 	return matrice;
 }
 
-parcours_comp()
+int* marquer_sommet(int *matrice, int x, int y)
 {
+	matrice[x * SIZE + y] = 0;
+	matrice[y * SIZE + x] = 0;
 
+	return matrice;
 }
 
-parcours_prof()
+int* insert_tail(int* liste, int val)
 {
+	int i = 0;
 
+	while (liste[i] != 0)
+	{
+		i++;
+	}
+
+	liste[i] = val;
+
+	return liste;
 }
 
-main()
+void parcours_comp(int* matrice, int *marc, int val)
+{
+	int i;
+
+	marc[val] = 1;
+
+	printf("%d ", val+1);
+
+	for (i = 0; i < SIZE; i++)
+	{
+		if (matrice[val * SIZE + i] != 0 && marc[i] == 0)
+		{
+			parcours_comp(matrice, marc, i);
+		}
+	}
+}
+
+void parcours_prof(int *matrice, int *marc)
+{
+	int i;
+
+	for (i = 0; i < SIZE; i++)
+	{
+		marc[i] = 0;
+	}
+
+	for (i = 0; i < SIZE; i++)
+	{
+		if (marc[i] == 0)
+		{
+			parcours_comp(matrice, marc, i);
+		}
+	}
+}
+
+int main()
 {
 	int i = 0;
 
@@ -69,5 +143,9 @@ main()
 	matrice = ajoute_sommet(matrice, 7, 6);
 	matrice = ajoute_sommet(matrice, 8, 1);
 
-	affiche(matrice);
+	affiche_matrice(matrice);
+
+	int* marc = init_liste();
+
+	parcours_prof(matrice, marc);
 }
